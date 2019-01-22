1a17eefde41986b6c90028ad1d5ab6cc96b328f4
voi86 <46761814+voi86@users.noreply.github.com>
2019-01-22 14:50:00 -0800

    Add files via upload

---
+++ ex_8.5.py
@@ -0,0 +1,12 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    if not line.startswith("From:"): continue
+    new=line.split()
+    print(new[1].rstrip())
+    count = count + 1
+print("There were", count, "lines in the file with From as the first word")
